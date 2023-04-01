a69619ab0500c014549654f7a757c5fda8c39b24
Nat456 <nat456.pro@gmail.com>
2023-04-01 16:04:51 +0200

    extension-update-2

--- extension/webtoon-extension/scripts/homepage.js
+++ extension/webtoon-extension/scripts/homepage.js
@@ -1,9 +1,10 @@
 const header = document.getElementById("header");
 const header_read = document.getElementById("_toolBarRightArea");
-const lang = window.location.href.split("/")[3];
 
+const lang = window.location.href.split("/")[3];
 let link = document.createElement('a');
 link.setAttribute('href', '/'+lang+'/favorite');
+link.setAttribute('title', 'Favorites');
 link.innerHTML = "+";
 
 if (header && false){
@@ -15,4 +16,14 @@ if (header && false){
     link.className = "favorite-link-read";
     const bar = document.getElementsByClassName("spi_area")[0];
     header_read.insertBefore(link, bar);
+
+    const next_url = document.getElementsByClassName("pg_next")[0].href;
+    if(next_url){
+        let next_link = document.createElement('a');
+        next_link.setAttribute('href', next_url);
+        next_link.setAttribute('title', 'NextEpisode');
+        next_link.className = "next-ep-read";
+        next_link.innerHTML = "NEXT";
+        document.getElementById("container").insertBefore(next_link, document.getElementById("_topBtn"))
+    }
 }
