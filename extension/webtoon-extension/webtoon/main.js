2b18654d1a67ae401c56671d15287027d61a8d11
Nat456 <nat456.pro@gmail.com>
2023-05-05 16:15:22 +0200

    bigga commit

--- extension/webtoon-extension/webtoon/main.js
+++ extension/webtoon-extension/webtoon/main.js
@@ -1,48 +1,41 @@
 const lang = window.location.href.split("/")[3];
-let link = document.createElement('a');
-link.setAttribute('href', '/'+lang+'/favorite');
-link.setAttribute('title', 'Subscribed');
-link.innerHTML = "♥";
 
-const header_read = document.getElementById("_toolBarRightArea");
-/*const header = document.getElementById("header");
-if (header){
-    link.className = "favorite-link";
-    const bar = document.getElementsByClassName("bar")[0];
-    const btn_bar = document.getElementsByClassName("sta")[0];
-    btn_bar.insertBefore(link, bar);
-}else */if (header_read){
-    link.className = "favorite-link-read";
-    const bar = document.getElementsByClassName("spi_area")[0];
-    header_read.insertBefore(link, bar);
-
-
-    const next_url = document.getElementsByClassName("pg_next")[0].href;
-    if(next_url){
-        let next_link = document.createElement('a');
-        next_link.setAttribute('href', next_url);
-        next_link.setAttribute('title', 'Next Episode');
-        next_link.className = "next-ep-read";
-        next_link.innerHTML = '<p class="next-btn arrow">→</p> <p class="next-btn next">NEXT</p>';
-        document.getElementById("container").appendChild(next_link);
-    }else{
-        let link2 = document.createElement('a');
-        link2.setAttribute('href', '/'+lang+'/favorite');
-        link2.setAttribute('title', 'Subscribed');
-        link2.innerHTML = '<p style="line-height: 53px;">♥</p>';
-        link2.className = "next-ep-favorite-read";
-        document.getElementById("container").appendChild(link2);
+chrome.storage.sync.get({ first: false, second: false, third: false, fourth: false },(settings) => {
+    if (settings.first){
+        document.getElementById("noticeArea").remove();
+        document.getElementsByTagName("body")[0].classList.add("cleaner");
+    }if (settings.second){
+        document.getElementsByTagName("body")[0].classList.add("no-btn");
     }
-}
+    const header_read = document.getElementById("_toolBarRightArea");
+    if (header_read){
+        let link = document.createElement('a')
+        link.setAttribute('href', `/${lang}/favorite`)
+        link.setAttribute('title', 'Subscribed')
+        link.className = "favorite-link-read"
+        link.innerHTML = "♥";
+        header_read.insertBefore(link, document.getElementsByClassName("spi_area")[0]);
 
-/*this.addEventListener('keypress', event => {
-    if (event.key === "n") {
-        if(next_url){
-            window.location.href = next_url;
-        }else{
-            window.location.href = '/'+lang+'/favorite';
+        const next_url = document.getElementsByClassName("pg_next")[0].href;
+        if(next_url){ 
+            if(settings.third){
+                let next_link = document.createElement('a');
+                next_link.setAttribute('href', next_url);
+                next_link.setAttribute('title', 'Next Episode');
+                next_link.className = "next-ep-read";
+                next_link.innerHTML = '<p class="next-btn arrow">→</p> <p class="next-btn next">NEXT</p>';
+                document.getElementById("container").appendChild(next_link);
+            }
+        }else if (settings.fourth){
+            let link2 = document.createElement('a');
+            link2.setAttribute('href', '/'+lang+'/favorite');
+            link2.setAttribute('title', 'Subscribed');
+            link2.innerHTML = '<p style="line-height: 53px;">♥</p>';
+            link2.className = "next-ep-favorite-read";
+            document.getElementById("container").appendChild(link2);
         }
+        addEventListener('keypress', event => {
+            if (event.key === "n") window.location.href = next_url ? next_url : `/${lang}/favorite`;
+        })
     }
-  })*/
-
-document.getElementById("noticeArea").remove();
+});
