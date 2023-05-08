ebe3ca74c8c6b2e6f34272661c76ca687fd6f1bd
Nat456 <nat456.pro@gmail.com>
2023-05-08 13:26:40 +0200

    error log

--- loup-garou/main.js
+++ loup-garou/main.js
@@ -11,16 +11,16 @@ function update_players_number(players){
         players = Math.round(players);
         if (players > 10) players = 10;
         else if (players < 4) players = 4;
-        document.getElementById('players_number').value = players
+        document.getElementById('players_number').value = players;
         old_players = players;
         setCookie("players",players);
 
         document.getElementById("players_container").innerHTML = '';
         for (let i = 1; i <= players; i++) {
-            let player = document.createElement("input")
-            player.setAttribute("type","text")
-            player.setAttribute("placeholder","Joueur "+i)
-            player.setAttribute("id","player"+i)
+            let player = document.createElement("input");
+            player.setAttribute("type","text");
+            player.setAttribute("placeholder","Joueur "+i);
+            player.setAttribute("id","player"+i);
             document.getElementById('players_container').appendChild(player);
         }
     }
@@ -34,14 +34,23 @@ function update_roles_number(roles){
         roles = Math.round(roles);
         if (roles > 3) roles = 3;
         else if (roles < 1) roles = 1;
-        document.getElementById('roles_number').value = roles
-        var nb_roles = document.getElementById('players_number').value * roles;
+        document.getElementById('roles_number').value = roles;
         old_roles = roles;
         setCookie("roles",roles);
     }
 }
 
 function start_game(){
-    document.getElementById("home").style.display = "none";
-    document.getElementById("game").style.display = "block";
+    let test = true
+    for (let i = 1; i <= document.getElementById('players_number').value; i++) {
+        if (! document.getElementById('player'+i).value){
+            test = false
+        }
+    }
+    if (test){
+        document.getElementById("home").style.display = "none";
+        document.getElementById("game").style.display = "block";
+    }else{
+        alert("Nomme bien tous les joueurs !")
+    }
 }
