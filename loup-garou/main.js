5ea6021c0eb7175a82b3a19033e15ade0de11578
Nat456 <nat456.pro@gmail.com>
2023-05-10 21:57:36 +0200

    not working but it will come soon

--- loup-garou/main.js
+++ loup-garou/main.js
@@ -1,8 +1,7 @@
 window.onload = () => {
-    if(getCookie("players")){
-        document.getElementById('players_number').value = Number(getCookie("players"));
-        document.getElementById('roles_number').value = Number(getCookie("roles"));
-    }
+    if(getCookie("players")) document.getElementById('players_number').value = Number(getCookie("players"));
+    if(getCookie("roles")) document.getElementById('roles_number').value = Number(getCookie("roles"));
+    update()
 };
 
 let old_players = 0;
@@ -60,9 +59,39 @@ function start_game(){
         }
     }
     if (test){
+        for (let i = 1; i <= getCookie("players"); i++) {
+            create_card(i,getCookie("players"));
+        }
         document.getElementById("home").style.display = "none";
         document.getElementById("game").style.display = "block";
     }else{
         alert("Nomme bien tous les joueurs !")
     }
+}
+
+function create_card(player,total){
+    let pos = circumference()/total*player;
+    let angle = (360/total)*player //calc_angle(circumference()/total*player) 
+    let card = document.createElement("button");
+    card.setAttribute("class","btn_player");
+    card.setAttribute("id","btn_player"+player);
+    card.style.position = "absolute";
+    //card.style.bottom = String(window.innerHeight/2 + Math.cos(angle*Math.PI/180)*window.innerHeight/3 - 50)+"px";
+    //card.style.left = String(window.innerWidth/2 + Math.sin(angle*Math.PI/180)*window.innerWidth/3 - 50)+"px";
+    card.style.bottom = String(window.innerHeight/2 + window.innerHeight/3*Math.sin(pos) - 50);
+    card.style.left = String(window.innerWidth/2 + window.innerWidth/3*Math.cos(pos) - 50);
+    card.style.zIndex = player;
+    card.style.transform =  "rotate("+String(angle+180)+"deg)";
+    card.innerHTML ='<img id="img_player'+player+'" src="images/back.png"><h2 class="player-name">'+document.getElementById("player"+String(player)).value+'</h2>';
+    document.getElementById('game_player_container').appendChild(card);
+}
+
+function calc_angle(pos){
+    return Math.atan2(window.innerHeight/3*Math.sin(pos),window.innerWidth/3*Math.cos(pos))
+}
+
+function circumference(){
+    let a = window.innerHeight/3
+    let b = window.innerWidth/3
+    return Math.PI*(3*(a+b)-Math.sqrt((3*a+b)*(a+3*b)))
 }
